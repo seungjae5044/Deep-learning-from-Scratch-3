9c26a149e91cba58a9f598c81e682ee00cd9547b
Seungjae Jeon <seungjae5044@gmail.com>
2024-11-30 03:38:47 +0900

    step04

--- steps/step.py
+++ steps/step.py
@@ -21,14 +21,20 @@ class Exp(Function):
     def forward(self, x):
         return np.exp(x)
     
+def numerical_diff(f, x, eps = 1e-4):
+    x0 = Variable(x.data-eps)
+    x1 = Variable(x.data+eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
 
-if __name__ == "__main__":
+def f(x):
     A = Square()
     B = Exp()
     C = Square()
+    return C(B(A(x)))
 
+if __name__ == "__main__":
     x = Variable(np.array(0.5))
-    a = A(x)
-    b = B(a)
-    y = C(b)
-    print(y.data)
+    dy = numerical_diff(f, x)
+    print(dy)
