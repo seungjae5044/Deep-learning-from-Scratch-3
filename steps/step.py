f7be81ca58444b5bee1751e2a54d1478b2c0a484
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-01 02:10:42 +0900

    stpe08: more convenient

--- steps/step.py
+++ steps/step.py
@@ -2,6 +2,10 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.'.format(type(data)))
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -10,17 +14,23 @@ class Variable:
         self.creator = func
 
     def backward(self):
-        f = self.creator
-        if f is not None:
-            x = f.input
-            x.grad = f.backward(self.grad)
-            x.backward()
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            x, y = f.input, f.output
+            x.grad = f.backward(y.grad)
+
+            if x.creator is not None:
+                funcs.append(x.creator)
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
@@ -49,24 +59,20 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
-    
-def numerical_diff(f, x, eps = 1e-4):
-    x0 = Variable(x.data-eps)
-    x1 = Variable(x.data+eps)
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2*eps)
+
+def square(x):
+    return Square()(x)
+
+def exp(x):
+    return Exp()(x)
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
 
 if __name__ == "__main__":
-    A = Square()
-    B = Exp()
-    C = Square()
-
     x = Variable(np.array(0.5))
-    a = A(x)
-    b = B(a)
-    y = C(b)
-
-    y.grad = np.array(1.0)
+    y = square(exp(square(x)))
     y.backward()
     print(x.grad)
