f4dc61c11488341d4da1125c50af22fb205b8657
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-01 01:27:26 +0900

    step06: backward

--- steps/step.py
+++ steps/step.py
@@ -3,23 +3,39 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         ouput = Variable(y)
+        self.input = input
         return ouput
+    
     def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):
         raise NotImplementedError()
 
 class Square(Function):
     def forward(self, x):
         return x ** 2
     
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
+    
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
     
 def numerical_diff(f, x, eps = 1e-4):
     x0 = Variable(x.data-eps)
@@ -28,13 +44,18 @@ def numerical_diff(f, x, eps = 1e-4):
     y1 = f(x1)
     return (y1.data - y0.data) / (2*eps)
 
-def f(x):
+if __name__ == "__main__":
     A = Square()
     B = Exp()
     C = Square()
-    return C(B(A(x)))
 
-if __name__ == "__main__":
     x = Variable(np.array(0.5))
-    dy = numerical_diff(f, x)
-    print(dy)
+    a = A(x)
+    b = B(a)
+    y = C(b)
+
+    y.grad = np.array(1.0)
+    b.grad = C.backward(y.grad)
+    a.grad = B.backward(b.grad)
+    x.grad = A.backward(a.grad)
+    print(x.grad)
