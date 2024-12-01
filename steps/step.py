68b27ed9624ef7a286247d7fe3315afc6d45d3d5
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-01 15:03:42 +0900

    step12: dynamic length parameter

--- steps/step.py
+++ steps/step.py
@@ -27,16 +27,18 @@ class Variable:
                 funcs.append(x.creator)
 
 class Function:
-    def __call__(self, inputs):
+    def __call__(self, *inputs):
         xs = [x.data for x in inputs]
-        ys = self.forward(xs)
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys =  (ys, )
         outputs = [Variable(as_array(y)) for y in ys]
 
         for output in outputs:
             output.set_creator(self)
         self.inputs = inputs
         self.outputs = outputs
-        return outputs
+        return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, x):
         raise NotImplementedError()
@@ -63,16 +65,18 @@ class Exp(Function):
         return gx
     
 class Add(Function):
-    def forward(self, xs):
-        x0, x1 = xs
+    def forward(self, x0, x1):
         y = x0 + x1
-        return (y, )
+        return y
     
 def square(x):
     return Square()(x)
 
 def exp(x):
     return Exp()(x)
+
+def add(x0, x1):
+    return Add()(x0, x1)
 
 def as_array(x):
     if np.isscalar(x):
@@ -87,10 +91,9 @@ def numerical_diff(f, x, eps = 1e-4):
     return (y1.data - y0.data)/(2*eps)
 
 if __name__ == "__main__":
-    xs = [Variable(np.array(2)), Variable(np.array(3))]
-    f = Add()
-    ys = f(xs)
-    y = ys[0]
+    x0 = Variable(np.array(2))
+    x1 = Variable(np.array(3))
+    y = add(x0, x1)
     print(y.data)
 
 
