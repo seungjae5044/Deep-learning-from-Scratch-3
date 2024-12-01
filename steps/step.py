a44d0553de91aa6a8f9e5e2f4aac58fbf628714b
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-01 14:56:57 +0900

    step11: natural code

--- steps/step.py
+++ steps/step.py
@@ -27,14 +27,16 @@ class Variable:
                 funcs.append(x.creator)
 
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
     
     def forward(self, x):
         raise NotImplementedError()
@@ -60,6 +62,12 @@ class Exp(Function):
         gx = np.exp(x) * gy
         return gx
     
+class Add(Function):
+    def forward(self, xs):
+        x0, x1 = xs
+        y = x0 + x1
+        return (y, )
+    
 def square(x):
     return Square()(x)
 
@@ -79,10 +87,14 @@ def numerical_diff(f, x, eps = 1e-4):
     return (y1.data - y0.data)/(2*eps)
 
 if __name__ == "__main__":
-    x = Variable(np.array(2.0))
-    y = square(x)
-    expected = np.array(4.0)
-    print(type(expected) , type(y.data))
+    xs = [Variable(np.array(2)), Variable(np.array(3))]
+    f = Add()
+    ys = f(xs)
+    y = ys[0]
+    print(y.data)
+
+
+#######Test_Code#######
 
 import unittest
 
