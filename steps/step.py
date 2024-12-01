4cc6d7de3dfc92cebc32a282d78d7e260fa29d3f
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-01 17:14:56 +0900

    step18: backprop mode

--- steps/step.py
+++ steps/step.py
@@ -1,5 +1,18 @@
 import numpy as np
 import weakref
+import contextlib
+
+class Config:
+    enable_backprop = True
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
 
 class Variable:
     def __init__(self, data):
@@ -19,7 +32,11 @@ class Variable:
     def cleargrad(self):
         self.grad = None
 
-    def backward(self):
+    def backward(self,retain_grad=False):
+
+        if not Config.enable_backprop:
+            raise SystemError('Config.using_backprop 가 {}에서는 backward를 동작할 수 업습니다.'.format(Config.enable_backprop))
+
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
@@ -49,6 +66,10 @@ class Variable:
 
                 if x.creator is not None:
                     add_func(x.creator)
+            
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None
 
 class Function:
     def __call__(self, *inputs):
@@ -58,11 +79,12 @@ class Function:
             ys =  (ys, )
         outputs = [Variable(as_array(y)) for y in ys]
 
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = [weakref.ref(output) for output in outputs]
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
         return outputs if len(outputs) > 1 else outputs[0]
     
     def forward(self, x):
@@ -111,6 +133,9 @@ def as_array(x):
         return np.array(x)
     return x
 
+def no_grad():
+    return using_config('enable_backprop', False)
+
 def numerical_diff(f, x, eps = 1e-4):
     x0 = Variable(x.data-eps)
     x1 = Variable(x.data+eps)
@@ -118,11 +143,10 @@ def numerical_diff(f, x, eps = 1e-4):
     y1 = f(x1)
     return (y1.data - y0.data)/(2*eps)
 
-if __name__ == "__main__":
-    x = Variable(np.random.randn(10000))
-    y = square(square(square(x)))
-    
-
+if __name__ == "__main__": 
+    with no_grad():
+        x = Variable(np.array(2.0))
+        y = square(x)
 
 #######Test_Code#######
 
