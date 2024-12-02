9c3492348339d3cd84f0886495d9d467c016baeb
Seungjae Jeon <seungjae5044@gmail.com>
2024-12-02 11:54:07 +0900

    step25-26: visualization

--- steps/step.py
+++ steps/step.py
@@ -4,6 +4,7 @@ if '__file__' in globals():
 
 import numpy as np
 from dezero import Variable
+from dezero.utils import plot_dot_graph
 
 def sphere(x, y):
     z = x ** 2 + y ** 2
@@ -18,8 +19,12 @@ def goldstein(x,y):
         (30+(2*x-3*y)**2*(18-32*x+12*x**2 + 48*y - 36*x*y + 27*y**2))
     return z
 
-x = Variable(np.array(1.0))
-y = Variable(np.array(1.0))
-z = goldstein(x, y)
-z.backward()
-print(x.grad, y.grad)
+if __name__ == "__main__":
+    x = Variable(np.array(1.0))
+    y = Variable(np.array(1.0))
+    z = goldstein(x, y)
+
+    x.name = 'x'
+    y.name = 'y'
+    z.name = 'z'
+    plot_dot_graph(z, verbose=False, to_file='goldstein.png')
